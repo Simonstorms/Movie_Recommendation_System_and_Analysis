cbeb6d2a2149c87fa7e55d27005e2421173193a2
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-11-14 18:47:21 +0100

    delete Optimization algorithm + added types
    the algorithm always weighted themes to high and cast to low. subjective i found the first the best recommandations

--- recommendation_system.py
+++ recommendation_system.py
@@ -1,9 +1,9 @@
+from typing import List, Dict, Set, Tuple, Optional, Union
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from scipy.sparse import hstack
-from scipy.optimize import minimize
+from scipy.sparse import hstack, csr_matrix
 
 class MovieRecommender:
     """
@@ -12,16 +12,15 @@ class MovieRecommender:
     """
 
     # Constants for ratings
-    MIN_RATING = 2
-    RATING_SCALE = 2
+    MIN_RATING: float = 2
+    RATING_SCALE: float = 2
 
-    def __init__(self):
-        self.movies_df = None
-        self.tfidf_matrices = {}
-        self.features = ['genre', 'theme', 'actors', 'description', 'tagline', 'director']
-        self.feature_weights = {feature: 1.0 for feature in self.features}
+    def __init__(self) -> None:
+        self.movies_df: Optional[pd.DataFrame] = None
+        self.tfidf_matrices: Dict[str, csr_matrix] = {}
+        self.features: List[str] = ['genre', 'theme', 'actors', 'description', 'tagline', 'director']
 
-    def load_data(self):
+    def load_data(self) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         """Load all necessary datasets"""
         return (
             pd.read_csv('movie_dataset/movies.csv'),
@@ -31,7 +30,14 @@ class MovieRecommender:
             pd.read_csv('movie_dataset/crew.csv'),
         )
 
-    def merge_datasets(self, movies, actors, genres, themes, crew):
+    def merge_datasets(
+        self,
+        movies: pd.DataFrame,
+        actors: pd.DataFrame,
+        genres: pd.DataFrame,
+        themes: pd.DataFrame,
+        crew: pd.DataFrame
+    ) -> pd.DataFrame:
         """Merge and preprocess all datasets"""
         movies = self._merge_genres(movies, genres)
         movies = self._merge_themes(movies, themes)
@@ -40,29 +46,29 @@ class MovieRecommender:
         movies = self._clean_movies(movies)
         return movies
 
-    def _merge_genres(self, movies, genres):
+    def _merge_genres(self, movies: pd.DataFrame, genres: pd.DataFrame) -> pd.DataFrame:
         """Merge genres into movies DataFrame"""
         genres_grouped = genres.groupby('id')['genre'].apply(' '.join).reset_index()
         return movies.merge(genres_grouped, on='id', how='left')
 
-    def _merge_themes(self, movies, themes):
+    def _merge_themes(self, movies: pd.DataFrame, themes: pd.DataFrame) -> pd.DataFrame:
         """Merge themes into movies DataFrame"""
         themes_grouped = themes.groupby('id')['theme'].apply(' '.join).reset_index()
         return movies.merge(themes_grouped, on='id', how='left')
 
-    def _merge_actors(self, movies, actors):
+    def _merge_actors(self, movies: pd.DataFrame, actors: pd.DataFrame) -> pd.DataFrame:
         """Merge actors into movies DataFrame"""
         actors_grouped = actors.groupby('id')['name'].apply(lambda x: ' '.join(x.astype(str))).reset_index()
         return movies.merge(actors_grouped, on='id', how='left')
 
-    def _merge_directors(self, movies, crew):
+    def _merge_directors(self, movies: pd.DataFrame, crew: pd.DataFrame) -> pd.DataFrame:
         """Merge directors into movies DataFrame"""
         directors = crew[crew['role'] == 'Director']
         crew_grouped = directors.groupby('id')['name'].apply(lambda x: ' '.join(x.astype(str))).reset_index()
         crew_grouped.rename(columns={'name': 'director'}, inplace=True)
         return movies.merge(crew_grouped, on='id', how='left')
 
-    def _clean_movies(self, movies):
+    def _clean_movies(self, movies: pd.DataFrame) -> pd.DataFrame:
         """Clean and preprocess the movies DataFrame"""
         movies.rename(columns={'name_x': 'name', 'name_y': 'actors'}, inplace=True)
         movies = movies.dropna(subset=['rating'])
@@ -72,105 +78,44 @@ class MovieRecommender:
         movies.loc[:, string_columns] = movies[string_columns].fillna('')
         return movies
 
-    def build_feature_matrices(self):
+    def build_feature_matrices(self) -> None:
         """Build TF-IDF matrices for all features"""
         for feature in self.features:
             tfidf = TfidfVectorizer(stop_words='english' if feature in ['tagline', 'description'] else None)
             self.tfidf_matrices[feature] = tfidf.fit_transform(self.movies_df[feature].fillna(''))
 
-    def create_user_profile(self, user_movies):
+    def create_user_profile(self, user_movies: pd.DataFrame) -> np.ndarray:
         """Create a user profile based on ratings and movie features"""
         weighted_profiles = self._calculate_weighted_profiles(user_movies)
         return self._normalize_profile(weighted_profiles)
 
-    def _calculate_weighted_profiles(self, user_movies):
+    def _calculate_weighted_profiles(self, user_movies: pd.DataFrame) -> csr_matrix:
         """Calculate weighted profiles for user movies"""
-        feature_matrices = [
-            self.tfidf_matrices[feature][user_movies['row_index']].multiply(self.feature_weights[feature])
+        return hstack([
+            self.tfidf_matrices[feature][user_movies['row_index']]
             for feature in self.features
-        ]
-        weighted_profiles = hstack(feature_matrices)
-        adjusted_ratings = ((user_movies['rating'] - self.MIN_RATING) / self.RATING_SCALE).values.reshape(-1, 1)
-        weighted_profiles = weighted_profiles.multiply(adjusted_ratings)
-        return weighted_profiles
+        ]).multiply(((user_movies['rating'] - self.MIN_RATING) / self.RATING_SCALE).values.reshape(-1, 1))
 
-    def _normalize_profile(self, weighted_profiles):
+    def _normalize_profile(self, weighted_profiles: csr_matrix) -> np.ndarray:
         """Normalize the user profile"""
-        profile_sum = weighted_profiles.sum(axis=0)
-        norm = np.linalg.norm(profile_sum)
-        if norm == 0:
-            return np.zeros(profile_sum.shape[1])
-        return np.asarray(profile_sum / norm)[0]
+        return np.asarray(weighted_profiles.sum(axis=0).flatten() /
+                         np.linalg.norm(weighted_profiles.sum(axis=0).flatten()))[0]
 
-    def _error_function(self, weights, user_movies):
-        """Compute the error between predicted scores and actual ratings"""
-        self.feature_weights = dict(zip(self.features, weights))
-
-        errors = []
-        for idx, row in user_movies.iterrows():
-            other_movies = user_movies[user_movies.index != idx]
-            if other_movies.empty:
-                continue
-
-            user_profile = self.create_user_profile(other_movies)
-
-            feature_vectors = [
-                self.tfidf_matrices[feature][row['row_index']].multiply(self.feature_weights[feature])
-                for feature in self.features
-            ]
-            movie_vector = hstack(feature_vectors)
-            movie_vector = movie_vector.toarray()
-
-            similarity = cosine_similarity(user_profile.reshape(1, -1), movie_vector).flatten()[0]
-
-            predicted_rating = similarity * self.RATING_SCALE + self.MIN_RATING
-
-            actual_rating = row['rating']
-            error = (predicted_rating - actual_rating) ** 2
-            errors.append(error)
-
-        return sum(errors)
-
-    def optimize_feature_weights(self, user_movies):
-        """Optimize feature weights to minimize prediction error"""
-        initial_weights = [1.0] * len(self.features)
-        bounds = [(0, None)] * len(self.features)
-
-        result = minimize(
-            self._error_function,
-            initial_weights,
-            args=(user_movies,),
-            bounds=bounds,
-            method='L-BFGS-B'
-        )
-
-        self.feature_weights = dict(zip(self.features, result.x))
-        print("Optimized feature weights:", self.feature_weights)
-
-    def get_recommendations(self, user_ratings, n=10):
+    def get_recommendations(self, user_ratings: pd.DataFrame, n: int = 10) -> pd.DataFrame:
         """Generate movie recommendations based on user ratings"""
-        # Prepare user movies data
         user_movies = user_ratings.merge(self.movies_df[['id', 'name']], on='name', how='left')
         user_movies['row_index'] = user_movies['name'].apply(
             lambda x: self.movies_df[self.movies_df['name'] == x].index[0]
         )
 
-        # Optimize feature weights
-        self.optimize_feature_weights(user_movies)
-
-        # Generate recommendations
         user_profile = self.create_user_profile(user_movies)
-        tfidf_matrix_combined = hstack([
-            self.tfidf_matrices[feature].multiply(self.feature_weights[feature])
-            for feature in self.features
-        ])
+        tfidf_matrix_combined = hstack([self.tfidf_matrices[feature] for feature in self.features])
         similarity_scores = cosine_similarity(
             user_profile.reshape(1, -1),
             tfidf_matrix_combined
         ).flatten()
 
-        # Get top recommendations
-        already_rated_ids = set(user_movies['id'])
+        already_rated_ids: Set[int] = set(user_movies['id'])
         top_indices = [
             i for i, score in sorted(enumerate(similarity_scores), key=lambda x: x[1], reverse=True)
             if self.movies_df.iloc[i]['id'] not in already_rated_ids
@@ -178,15 +123,15 @@ class MovieRecommender:
 
         return self.movies_df.iloc[top_indices][['id', 'name', 'genre', 'rating']]
 
-    def fit(self):
+    def fit(self) -> 'MovieRecommender':
         """Initialize and prepare the recommendation system"""
-        # Load and prepare data
         movies_df, actors_df, genres_df, themes_df, crew_df = self.load_data()
         self.movies_df = self.merge_datasets(movies_df, actors_df, genres_df, themes_df, crew_df)
         self.movies_df = self.movies_df.reset_index(drop=True)
 
         self.build_feature_matrices()
         return self
+
 
 if __name__ == "__main__":
     recommender = MovieRecommender().fit()
